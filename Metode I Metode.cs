ff79c35ae003a325f7c5a337c7734cacbdcb9d9e
oliverornkow <34339613+oliverornkow@users.noreply.github.com>
2017-12-07 12:43:16 +0100

    Add files via upload

---
+++ Metode I Metode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HansOgGrethe();
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("Metode 2");
+        }
+
+
+        static void HansOgGrethe()
+        {
+            Console.WriteLine("Metode 1");
+            Test();
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
